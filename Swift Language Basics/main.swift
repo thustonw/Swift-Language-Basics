c59e789536402d74d07e9956efaac146b8856ee2
Wil Thuston <thustonw@gmail.com>
2017-09-04 18:20:04 -0500

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Wil C Thuston on 9/4/17.
+//  Copyright © 2017 Wil Thuston. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
